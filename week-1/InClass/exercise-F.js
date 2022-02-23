b4fb1f11d2245aba1ddd8a76d72e4972486a31ff
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-23 20:48:13 +0100

    adding the exercise F

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,9 @@
+const numberOfStudents = 15;
+
+const numberofMentors = 8;
+
+console.log(`Number of students:  ${numberOfStudents}.`);
+
+console.log(`Number of mentors:  ${numberofMentors}.`);
+
+console.log(`Total number of students and mentors:  ${numberOfStudents +numberofMentors}.`);
