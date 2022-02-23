fa1a0e6efd9199c0e22f76ddc2ea2b6731e658cf
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-23 21:03:49 +0100

    adding extra way to see on console the exercise F

--- week-1/InClass/exercise-F.js
+++ week-1/InClass/exercise-F.js
@@ -2,8 +2,13 @@ const numberOfStudents = 15;
 
 const numberofMentors = 8;
 
+const totalNumberOfClass = numberOfStudents + numberofMentors;
+
+
 console.log(`Number of students:  ${numberOfStudents}.`);
 
 console.log(`Number of mentors:  ${numberofMentors}.`);
 
-console.log(`Total number of students and mentors:  ${numberOfStudents +numberofMentors}.`);
+console.log(`Total number of students and mentors:  ${numberOfStudents + numberofMentors}.`);
+
+console.log('Total number of students and mentors:'  + totalNumberOfClass );
