3afd1f414ae2474508d3213818037b88a7963069
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 00:52:47 +0100

    adding the second part of the exercise -I

--- week-1/InClass/exercise-I.js
+++ week-1/InClass/exercise-I.js
@@ -5,22 +5,12 @@ function age(thisYears, yearsOld){
 const myBirthYear = age(2022, 34);
 console.log(myBirthYear); // prints 1988
 
+// Second part of the excercise:
 
-// function createGreeting(name, year) {
-//     let nascimento = age(34);
-//     let message = "Hello " + name + " You've born in " + nascimento + ".";
-//     return message;
-// }
+function greetings(name, year){
+    return message = ` Hello, my name is ${name} and I've born in ${year}.`
+};
 
-// let helloMessage = createGreeting(Cristiane, 1987)
+let greetingsFromBarcelona = greetings("Cristiane", myBirthYear);
 
-// console.log(helloMessage);
-
-// function greetings(name, year){
-//     let ageinYears = age(2022, 34);
-//     let message = " My name is $(name) and I was born in $(age).";
-//     return message;
-// }
-
-// let helloGreetings = greetings("cristiane", 1987);
-// console.log(greetings);
+console.log(greetingsFromBarcelona); // prints: Hello, my name is Cristiane and I've born in 1988.
