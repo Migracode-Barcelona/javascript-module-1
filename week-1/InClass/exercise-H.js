d7303ef4d17fea6391b466dd855269605f0f3ebe
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-25 23:21:00 +0100

    adding the answer of the questions of exercise-H

--- week-1/InClass/exercise-H.js
+++ week-1/InClass/exercise-H.js
@@ -1,3 +1,20 @@
-function names (nameA, nameB){
-    return nameA + nameB;
+function numbers (num1, num2){
+    return num1 + num2;
 }
+
+// The function it get the num1 and add numb2 
+
+const randomNumbers = numbers(5, 10)
+
+console.log(randomNumbers)
+
+/*  What's the difference between a return and console.log?
+    
+    - the return is a way to get the type of the paramethers, modify it for some specific value and also to execute some extra work in action of the function.
+    -   console.log - prints the result on the screen
+
+    When would you choose to use functions over the way we have been scripting so far?
+
+    -We choose the functions ( is a kind of block of codes)when we need to execute it repetitive times, so we do not need to write every single line and let the function do the job after giving it the correcter parameters.
+
+*/
