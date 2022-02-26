efdadda7924d472f55d02a2ba10d45343ae7c68f
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 01:41:00 +0100

    adding the answer of the exercise 1 F

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,8 @@
-const name = " Daniel  ";
+const nameUser = " Cristiane  ";
+let nameLength = nameUser.length;
+
+
+let message = `My name is ${nameUser.trim()} and my name have ${nameLength
+} characters long.`;
 
 console.log(message);
