15e34d285bd6487cbbe3e92b804102946a3d6f5d
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 01:52:27 +0100

    completed the exercise 2 of J 'functions' // comments added

--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,14 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
-var result = triple(12);
+var result = triple(12); // prints 6
 
-console.log(result);
+var result = triple(22); // prints 66
+
+var result = triple(222); // prints 666
+
+var result = triple(2222); // prints 6666
+
+console.log(result); 
