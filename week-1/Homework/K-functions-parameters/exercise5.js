7237d3ffaea5a82aa50d490940b247d872e2817b
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 16:01:41 +0100

    adding the correct space on the phrase

--- week-1/Homework/K-functions-parameters/exercise5.js
+++ week-1/Homework/K-functions-parameters/exercise5.js
@@ -5,4 +5,4 @@ function createLongGreeting( name, age ){
 
 const greeting = createLongGreeting("Daniel", 30);
 
-console.log(greeting); // prints: Hello, my name is Daniel and I'm 30years old.
+console.log(greeting); // prints: Hello, my name is Daniel and I'm 30 years old.
