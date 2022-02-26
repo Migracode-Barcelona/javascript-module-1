cf99033a87771540114511630c6046b06ecff5df
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 17:50:36 +0100

    making the code smaller using the better practice of the const variable

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,11 +1,9 @@
 // Start by creating a variable `message`
 
-let nameUser = "Cristiane";
-let nameLength = nameUser.length;
+const nameUser = "Cristiane";
+const nameLength = nameUser.length;
 
-let message = `My name is ${nameUser} and my name have ${nameLength
-} characters long.`;
-
-console.log(message);
+console.log(`My name is ${nameUser} and my name have ${nameLength
+} characters long.`);
 
 //Prints: My name is Cristiane and my name have 12 characters long.
