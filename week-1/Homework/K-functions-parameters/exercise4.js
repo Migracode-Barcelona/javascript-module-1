acb49ef8128341b0fa68639a49571d0d716d385e
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 02:10:33 +0100

    adding the function to the exercise - with comments - ex 4 - K

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,10 @@
 // Declare your function first
+function numbers(num1, num2){
+    return num1 + num2;
+}
 
 // Call the function and assign to a variable `sum`
 
-console.log(sum);
+const sum = numbers(13, 124);
+
+console.log(sum); // prints 137
