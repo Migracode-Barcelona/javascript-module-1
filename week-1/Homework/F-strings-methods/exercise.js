b2bbdaa3f18550d061481693bf88e2c94134fac0
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 01:25:24 +0100

    adding the answer of the exercise 1 F

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
 
+let nameUser = "Cristiane";
+let nameLength = nameUser.length;
+
+let message = `My name is ${nameUser} and my name have ${nameLength
+} characters long.`;
+
 console.log(message);
