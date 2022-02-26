5db37f95ed2cef0bca0252b085cb01c29b17cd0c
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 02:14:18 +0100

    adding the last exercise of K group - added comments - exercise 5

--- week-1/Homework/K-functions-parameters/exercise5.js
+++ week-1/Homework/K-functions-parameters/exercise5.js
@@ -1,5 +1,8 @@
 // Declare your function here
+function createLongGreeting( name, age ){
+    return `Hello, my name is ${name} and I'm ${age}years old.`;
+}
 
 const greeting = createLongGreeting("Daniel", 30);
 
-console.log(greeting);
+console.log(greeting); // prints: Hello, my name is Daniel and I'm 30years old.
