720426e4f13c6059a575e58df93041c62a40d587
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 02:29:04 +0100

    adding first part of the function on exercise L - nested functions

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function uperCaseName (name){
+    return `Hello ${name.toUpperCase()}`
+}
+// function uperCaseGreetings
+
+
+let greetings = uperCaseName("Daniel")
+
+console.log(greetings); // Prints Hi DANIEL
+
+
