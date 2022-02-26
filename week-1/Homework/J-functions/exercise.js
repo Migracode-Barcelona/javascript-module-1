7b30dffe5999f847358486eb9f863be2803e0683
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-26 01:49:38 +0100

    completed the exercise 1 of J 'functions' // comments added

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,16 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
-var result = halve(12);
+var result = halve(12); // prints 6
 
-console.log(result);
+var result = halve(17); // print 8.5
+
+var result = halve(1287); //prints 643.5
+
+var result = halve(20);  //prints 10
+
+console.log(result); 
+
+
