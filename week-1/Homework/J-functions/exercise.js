595e1d0804b9449385341ad60bf1d00f74ee6cc4
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-27 16:40:44 +0100

    changing the var for let

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -3,13 +3,13 @@ function halve(number) {
   return number / 2;
 }
 
-var result = halve(12); // prints 6
+let result = halve(12); // prints 6
 
-var result = halve(17); // print 8.5
+let result = halve(17); // print 8.5
 
-var result = halve(1287); //prints 643.5
+let result = halve(1287); //prints 643.5
 
-var result = halve(20);  //prints 10
+let result = halve(20);  //prints 
 
 console.log(result); 
 
