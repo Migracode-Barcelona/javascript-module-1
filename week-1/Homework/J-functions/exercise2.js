e6a94907478588300a85ec7bf84aef4cf117faac
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-27 16:40:52 +0100

    changing the var for let

--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -3,12 +3,12 @@ function triple(number) {
   return number * 3;
 }
 
-var result = triple(12); // prints 6
+let result = triple(12); // prints 6
 
-var result = triple(22); // prints 66
+let result = triple(22); // prints 66
 
-var result = triple(222); // prints 666
+let result = triple(222); // prints 666
 
-var result = triple(2222); // prints 6666
+let result = triple(2222); // prints 6666
 
 console.log(result); 
