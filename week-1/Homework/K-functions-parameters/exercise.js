e134a6d4274fa424792d1017d8e0308a667a7f5f
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-02-27 16:43:25 +0100

    changing the var for const

--- week-1/Homework/K-functions-parameters/exercise.js
+++ week-1/Homework/K-functions-parameters/exercise.js
@@ -5,6 +5,6 @@ function multiply(num1, num2) {
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+const result = multiply(3, 4);
 
 console.log(result); // prints 12
