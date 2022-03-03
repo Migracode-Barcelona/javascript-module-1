1e9543b3b7d95e9660718bf46ffdca70b8d95719
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-03 15:36:13 +0100

    exercise B completed, and answer for the questions added as coments

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -17,18 +17,21 @@ Node REPL:
 1.1 - As a class, can you step through the function and explain what each line does?
 
 
-LINE 1   <---- ginving a name to a function and adding parameter to the function
+LINE 1 <---- START: giving a name to a function  (boolChecker )and adding parameter to the name function (bool)
 
-LINE 3 <---- IF the type of the parameter (bool ) is exactly like a boolean ( primitive type that is both trye or false)
+LINE 3 <---- IF the type of the parameter (bool) is exactly like a boolean ( primitive type that is both trye or false),
 
-LINE 4 <---- It will return the message: You've given me a bool, thanks!
+LINE 4 <---- it will return the message: You've given me a bool, thanks!
 
 LINE 5 <----- IF the type of the parameter isnt a boolean ( can be number / string / objetc, etc),
-              it will return : No bool, not cool
+             
+LINE 6 <----- it will return : No bool, not cool.
+
+LINE 7  <----- End of the funcion.
 
 
 LINE 9 <----- We print to the console throught the command console.log, calling the funcion inside the parentheses.
-              inserting as an argument one of the results of a boolean to confirm if match or now in our funcion.
+              inserting as an argument after the name of the funcion inside another parentheses, one of the results of a boolean to confirm if match or now in our funcion.
 
               As a result checked on Node REPL, it prints
 
