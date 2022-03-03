92383cce7bc0bb3659781ad8b6ed9eee55492a22
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-03 13:49:37 +0100

    adding the exercise B to week 2

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -1,9 +1,20 @@
 function boolChecker(bool) {
-  if (typeof bool === ) {
+  const boolChecker = 'boolean';
+
+  if (typeof bool === bool) {
     return "You've given me a bool, thanks!";
   }
-
   return "No bool, not cool.";
 }
 
-boolChecker(true);
+console.log(boolChecker()); // prints : Not bool, not cool
+
+/*
+Node REPL:
+
+1- In a node REPL, what is the typeof a true or false?
+   The type of true and false are both 'boolean' - primitive type
+
+*/
+
+// FALTA CORRIGIR, ESTA DANDO ERRADO
