9d1d8cf749d6e660a3823ec1541eb5be29f0a9df
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-03 16:11:55 +0100

    correcting an incorrect space on the exercise

--- week-2/InClass/exercise-E.js
+++ week-2/InClass/exercise-E.js
@@ -3,6 +3,6 @@ num > 5 && num < 15; //true
 num < 10 || num === 10; // true;
 false || true; // true;
 !true  // false
-let greaterThan 5 = num > 5 // true;
+let greaterThan5 = num > 5; // true;
 !greaterThan5 /* ! implica lo contrario - falso; */
 !(num === 10); /* ! implica lo comtrario - falso; */
