306959c9376e142c29914f88e759ba7d2233a3e5
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-05 15:19:28 +0100

    adding the exercise-H - Still need to correct

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -1,3 +1,13 @@
+// function exponential(number) {
+//   return number * number;
+// }
+
+// function isEven(number) {
+//   return number % 2 === 0;
+// }
+
+// 
+
 function exponential(number) {
   return number * number;
 }
@@ -5,3 +15,21 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for (let i = 5; i < 20; i++){
+    console.log(`The exponential of ${isEven()} is ${exponential()}`)
+    isEven = isEven + 2;
+}
+
+/*
+Prints
+
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+The exponential of true is 36
+*/
