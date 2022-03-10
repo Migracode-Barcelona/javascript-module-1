6665338a29df54accdcbfff1c288ce16ff19066c
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-10 18:29:33 +0100

    adding exercise-A Inclass

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,18 @@
+let peopleOnMyTable = ["Cristiane", "Rosangela", "Marina"];
+
+console.log(peopleOnMyTable, peopleOnMyTable.length); 
+
+// prints :
+
+// 'Cristiane', 'Rosangela', 'Marina' ] 3
+
+peopleOnMyTable.unshift("Felix"); // Añade el nombre al inicio del array
+peopleOnMyTable.push('SomeoneElse') // Añade el nombre al final del Array
+
+
+console.log(peopleOnMyTable); 
+
+// prints :
+
+// [ 'Felix', 'Cristiane', 'Rosangela', 'Marina', 'SomeoneElse' ]
+
