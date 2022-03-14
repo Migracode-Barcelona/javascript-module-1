cbe85725f5cd6e7e5bbaea85e1ca210903bfa093
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-14 22:43:26 +0100

    adding the exercise E of the homework

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -3,3 +3,34 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+//solution 1 - callback function:
+
+/*
+let numbersMultiplied = numbers.map(function (number){
+    return number * 100
+})
+
+console.log(numbersMultiplied) // prints: [ 10, 20, 30, 40, 50 ]
+
+*/
+
+//solution 2
+/*
+
+let numbersMultiplied = numbers.map(number => {
+    return number * 100
+}) 
+
+console.log(numbersMultiplied)  // prints: [ 10, 20, 30, 40, 50 ]
+*/
+
+//solution 3
+
+
+let numbersMultiplied = numbers.map(number => number * 100) 
+
+console.log(numbersMultiplied)  // prints: [ 10, 20, 30, 40, 50 ]
+
+
+
+
