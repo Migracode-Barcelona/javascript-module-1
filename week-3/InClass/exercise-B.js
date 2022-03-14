16c30b51f40ba6fe20c13a8ebc471529efc6d3ab
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-14 23:40:40 +0100

    adding exercise B inclass

--- week-3/InClass/exercise-B.js
+++ week-3/InClass/exercise-B.js
@@ -1,3 +1,43 @@
- lalalala
+let peopleOnMyClass = ["Cristiane" , "Valeria" , "Diego" , "Alejandra" , "Yennifer" , "Angel "];
 
- teste 
+let peopleFromTheOtherClass = peopleOnMyClass.concat("Yogi ", "Diana ", "Abdul ", "Carlos ", "Vincent ", "David ")
+
+console.log(peopleFromTheOtherClass.sort())
+
+
+/* prints  
+[
+  'Abdul ',     'Alejandra',
+  'Angel ',     'Carlos',
+  'Cristiane ', 'David',
+  'Diana',     'Diego',
+  'Valeria',   'Vincent',
+  'Yennifer',  'Yogi'
+]
+*/
+
+// function callingByTheName(people){
+//     if (peopleOnMyClass == people) {
+//         return `${people} is at the class with ${peopleOnMyClass}`
+//     } 
+//         return `${people} is not at the class with ${peopleOnMyClass}`
+
+// }
+
+
+
+// prints:
+
+// Vincent is not at the class with Cristiane,Valeria,Diego,Alejandra,Yennifer,Angel
+
+function callingByTheName(name, arr){
+    if (!arr.includes(name)){
+        return `${name} is not at the class with ${peopleOnMyClass}`
+    }
+    return  `${name} is at the class with ${peopleOnMyClass}`
+}
+
+console.log(callingByTheName("Juan", peopleFromTheOtherClass))
+
+
+// prints: Juan is not at the class with Cristiane,Valeria,Diego,Alejandra,Yennifer,Angel 
