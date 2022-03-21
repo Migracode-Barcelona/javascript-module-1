f329c1fd658360711e158ab9fc00f3b3c01e3d9d
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-03-22 00:15:41 +0100

    adding exercise-f - asked help for the teacher, waiting the answer

--- week-3/InClass/exercise-F.js
+++ week-3/InClass/exercise-F.js
@@ -1,13 +1,72 @@
 /*
 
 You can drive in the UK at the age of 17.
-Write another function that takes a birth year and returns a string Born in {year} can drive or Born in {year} can drive in {x} years
-Use the array of birth years, [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ], to get an array of strings saying if these people can drive
+
+Write another function that takes a birth year and returns a string Born in {year}
+ can drive or Born in {year} can drive in {x} years
+
+Use the array of birth years, [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ],
+ to get an array of strings saying if these people can drive
+
 console.log the answers
 
 
 */
 
+const birthYears = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
+
+// const ages = birthYears.map(function (birthYear){
+//     const year = 2022;
+//     const yearsOld = year - birthYear;
+//     return yearsOld
+
+// })
+
+// solucion 1 cris:
+
+// const dLicense = birthYears.map(function (driver){
+//     const year = 2022;
+//     const yearsOld = year - driver;
+//     const legalDriver = yearsOld >= 17;
+//     const waitingPermition = yearsOld < 17;
+//     const yearsWaiting = 17 - yearsOld;
+//     if (driver === legalDriver){
+//         console.log( `Born in ${birthYears.length()} can drive`)
+//     } else ( driver === waitingPermition)
+//         console.log(`Born in ${birthYears} can drive in ${yearsWaiting} years`)
+//     });
 
 
-// completar
+
+// Prints:
+
+/*
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in -41 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in 3 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in -6 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in 0 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in -27 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in -20 years
+Born in 1964,2008,1999,2005,1978,1985,1919 can drive in -86 years
+
+*/
+
+
+const dLicense = birthYears.map(function (driver){
+    const year = 2022;
+    const yearsOld = year - driver;
+    const legalDriver = yearsOld >= 17;
+    const waitingPermition = yearsOld < 17;
+    const yearsWaiting = 17 - yearsOld;
+    if ( driver == waitingPermition){
+         console.log(`Born in ${birthYears.length} can drive in ${yearsWaiting} years`)
+    } else (driver == legalDriver) 
+        console.log( `Born in ${birthYears.length} can drive`)
+       
+    });
+
+
+  
+// PEDI AYUDA AL PROFESOR ALEJANDRO
+
+
