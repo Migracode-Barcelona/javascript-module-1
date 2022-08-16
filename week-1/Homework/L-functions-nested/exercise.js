62acd553c4a176379deaa71957d27abe9a73a772
Amanda Maria <amanda.moov@gmail.com>
2022-08-16 22:52:50 +0200

    finish HW

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,29 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var mentor1 = "Lucia";
+var mentor2 = "Giorgio";
+var mentor3 = "Fred";
+var mentor4 = "Ananda";
+var mentor5 = "Ali";
+
+function upperCase(name) {
+    return name.toUpperCase();
+    
+}
+
+// let mentorUpper = upperCase(mentor1);
+// console.log(mentorUpper);
+
+
+function greeting(mentor){
+    let mentorUpper = upperCase(mentor);
+    console.log(`HELLO ${mentorUpper}`)
+    // return `HELLO ${mentorUpper}`
+}
+
+// let hello = greeting(mentor1)
+// console.log(hello);
+
+greeting(mentor1);
+greeting(mentor2);
+greeting(mentor3);
+greeting(mentor4);
+greeting(mentor5);
