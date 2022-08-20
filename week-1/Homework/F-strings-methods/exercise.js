e50b2862d520cf93f0740c72d31ae09732d4b601
Amanda Maria <amanda.moov@gmail.com>
2022-08-20 12:03:29 +0200

    add function on wee1k-HW-F-string-1

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,6 +1,11 @@
 // Start by creating a variable `message`
-let message = 'Amanda'
-console.log(message); 
+// let message = 'Amanda'
+// console.log(message); 
 
-let lengthMessage = message.length;
-console.log(`My name is ${message} ant the length of my name is ${lengthMessage}.`);
+function lengthMessage(message) {
+    let lengthMessage = message.length;
+    console.log(`My name is ${message} ant the length of my name is ${lengthMessage}.`);
+}
+
+lengthMessage("amanda");
+
